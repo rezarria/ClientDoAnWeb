af0cbc67b4dde95e1f20f0f925319644a99a69d7
rezarria <votanamcb@outlook.com>
2022-12-03 20:16:39 +0700

    Thêm controller PhongHoc và LopHoc

---
+++ Areas/Admin/Controllers/PhongHoc.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class PhongHoc : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
