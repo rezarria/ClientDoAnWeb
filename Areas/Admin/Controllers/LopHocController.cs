e8b87498555174486ed7342a0add74e3c43ea6d5
rezarria <votanamcb@outlook.com>
2022-12-07 01:54:17 +0700

    up

--- Areas/Admin/Controllers/LopHocController.cs
+++ Areas/Admin/Controllers/LopHocController.cs
@@ -5,9 +5,9 @@ namespace Client.Areas.Admin.Controllers;
 [Area("Admin")]
 public class LopHocController : Controller
 {
-	public IActionResult Index()
-	{
-		return View();
-	}
+    public IActionResult Index()
+    {
+        return View();
+    }
 }
 
