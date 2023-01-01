e5ae7ba6c518433e408e70b68a8899674873a176
rezarria <votanamcb@outlook.com>
2023-01-01 07:24:45 +0700

    up

--- Areas/Admin/Views/Shared/Components/Sidebar/Sidebar.cs
+++ Areas/Admin/Views/Shared/Components/Sidebar/Sidebar.cs
@@ -1,6 +1,8 @@
 #region
 
+using Client.Areas.Admin.Contexts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 #endregion
 
@@ -8,8 +10,36 @@ namespace Client.Areas.Admin.Views.Shared.Components.Sidebar;
 
 public class Sidebar : ViewComponent
 {
-	public IViewComponentResult Invoke(dynamic data)
+
+	private ClientDbContext _context;
+
+	public Sidebar(ClientDbContext context)
 	{
-		return View();
+		_context = context;
+	}
+
+	public async Task<IViewComponentResult> InvokeAsync(dynamic _)
+	{
+		List<Models.SidebarNavItem> data = await (
+			from x in _context.SidebarNavItem
+			where x.ParentId == null
+			select x
+		)
+		.Include(x => x.Childs)
+		.AsNoTracking()
+		.ToListAsync(HttpContext.RequestAborted);
+
+		IQueryable<Models.SidebarNavItem> targets = data.SelectMany(x => x.Childs).AsQueryable();
+		List<Task> jobs = new List<Task>();
+		while (targets.Any())
+		{
+			await targets.ForEachAsync(x =>
+			{
+				jobs.Add(_context.Entry(x).Collection(y => y.Childs).LoadAsync(HttpContext.RequestAborted));
+			});
+			Task.WaitAll(jobs.ToArray(), HttpContext.RequestAborted);
+			targets = targets.SelectMany(x => x.Childs).AsQueryable();
+		}
+		return View(data);
 	}
 }
